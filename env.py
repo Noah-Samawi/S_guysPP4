6115af669c0a2e11024dee3018c0fe849088224b
Noah Samawi <noah.alsamawi@gmail.com>
2024-02-23 10:57:19 +0100

    Made change in gitignore file

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+# Other settings...
+
+# Static files (CSS, JavaScript, images)
+
+os.environ.setdefault("DATABASE_URL", "postgres://gazhqxww:zF090-TY-hKlkdoTCJdFq1xbxs3gE2Mz@tai.db.elephantsql.com/gazhqxww")
+os.environ.setdefault("SECRET_KEY", "IT#22#apfel#kiwi#2024")
